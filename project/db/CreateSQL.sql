1f3b0ae5cad3426bfe56fc154bea3b05576cccc5
kevin011602 <kevin.paladino02@gmail.com>
2024-01-16 11:30:34 +0100

    Aggiornato Database focusproject

---
+++ project/db/CreateSQL.sql
@@ -0,0 +1,140 @@
+DROP DATABASE focusproject;
+CREATE DATABASE IF NOT EXISTS focusproject;
+USE focusproject;
+
+DROP TABLE IF EXISTS Azienda;
+CREATE TABLE Azienda (
+    piva VARCHAR(11) NOT NULL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS Utente;
+CREATE TABLE Utente (
+    email VARCHAR(255) PRIMARY KEY,
+    pwd VARCHAR(255) NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    cognome VARCHAR(255) NOT NULL,
+    idAzienda VARCHAR(11) NOT NULL,
+    stato BOOLEAN DEFAULT TRUE,
+    ruolo VARCHAR(20) NOT NULL,
+    FOREIGN KEY (idAzienda) REFERENCES Azienda(piva)
+    ON UPDATE CASCADE
+	ON DELETE RESTRICT
+);
+
+DROP TABLE IF EXISTS Comunicazione;
+CREATE TABLE Comunicazione (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titolo VARCHAR(255),
+    corpo TEXT,
+    mittente_email VARCHAR(255) NOT NULL,
+    FOREIGN KEY (mittente_email) REFERENCES Utente(email)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+);
+
+DROP TABLE IF EXISTS Turno;
+CREATE TABLE Turno (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    giorno VARCHAR(255) NOT NULL,
+    ora_inizio VARCHAR(255) NOT NULL,
+    ora_fine VARCHAR(255) NOT NULL
+);
+
+-- Relazione Molti-a-Molti tra Turno e Utente
+DROP TABLE IF EXISTS AssegnatoA;
+CREATE TABLE AssegnatoA (
+    id_turno INT NOT NULL,
+    id_utente VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_turno) REFERENCES Turno(id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT,
+    FOREIGN KEY (id_utente) REFERENCES Utente(email)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+);
+
+DROP TABLE IF EXISTS Permesso;
+CREATE TABLE Permesso (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    dal_giorno VARCHAR(255) NOT NULL,
+    al_giorno VARCHAR(255) NOT NULL,
+    motivo VARCHAR(255) NOT NULL,
+    stato BOOLEAN,
+    richiedente_email VARCHAR(255) NOT NULL,
+    FOREIGN KEY (richiedente_email) REFERENCES Utente(email)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT
+);
+
+DROP TABLE IF EXISTS Progetto;
+CREATE TABLE Progetto (
+    id_progetto INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    descrizione VARCHAR(255) NOT NULL,
+    obbiettivi VARCHAR(255) NOT NULL,
+    scadenza VARCHAR(255) NOT NULL,
+    avvisi VARCHAR(255),
+    budget DOUBLE NOT NULL,
+    numDipendenti INT,
+    responsabile_email VARCHAR(255) NOT NULL,
+    stato BOOLEAN DEFAULT FALSE,
+    idAzienda VARCHAR(11) NOT NULL,
+    FOREIGN KEY (idAzienda) REFERENCES Azienda(piva)
+    ON UPDATE CASCADE
+	ON DELETE RESTRICT
+);
+
+-- Relazione Molti-a-Molti tra Subordinato e Progetto
+DROP TABLE IF EXISTS LavoraA;
+CREATE TABLE LavoraA (
+	email VARCHAR(255) NOT NULL,
+    id_progetto INT NOT NULL,
+    FOREIGN KEY (email) REFERENCES Utente(email)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT,
+    FOREIGN KEY (id_progetto) REFERENCES Progetto(id_progetto)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT
+);
+
+DROP TABLE IF EXISTS Task;
+CREATE TABLE Task (
+    id_task INT PRIMARY KEY AUTO_INCREMENT,
+    descrizione VARCHAR(255) NOT NULL,
+    stato BOOLEAN DEFAULT FALSE,
+    id_progetto INT NOT NULL,
+    subordinato_email VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_progetto) REFERENCES Progetto(id_progetto)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    FOREIGN KEY (subordinato_email) REFERENCES Utente(email)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);
+
+DROP TABLE IF EXISTS StatsResponsabile;
+CREATE TABLE StatsResponsabile (
+    email VARCHAR(255) PRIMARY KEY,
+	num_progetti_completati INT DEFAULT 0,
+    num_progetti_in_corso INT DEFAULT 0,
+    num_permessi_richiesti INT DEFAULT 0,
+    num_subordinati_gestiti INT DEFAULT 0,
+    num_scadenze_rispettate INT DEFAULT 0,
+    FOREIGN KEY (email) REFERENCES Utente(email)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT
+);
+
+DROP TABLE IF EXISTS StatsSubordinato;
+CREATE TABLE StatsSubordinato (
+    email VARCHAR(255) PRIMARY KEY,
+	num_progetti_completati INT DEFAULT 0,
+    num_progetti_in_corso INT DEFAULT 0,
+    num_permessi_richiesti INT DEFAULT 0,
+    num_task_completati INT DEFAULT 0,
+    num_task_in_corso INT DEFAULT 0,
+    FOREIGN KEY (email) REFERENCES Utente(email)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT
+);
---
+++ project/db/InsertSQL.sql
@@ -0,0 +1,48 @@
+USE focusproject;
+
+INSERT INTO Azienda (piva, nome) VALUES
+('12345678901', 'Azienda1'),
+('98765432101', 'Azienda2'),
+('11223344556', 'Azienda3');
+
+INSERT INTO Utente (email, pwd, nome, cognome, idAzienda, ruolo) VALUES
+('user1@example.com', 'password1', 'John', 'Doe', '12345678901', 'Dirigente'),
+('user2@example.com', 'password2', 'Jane', 'Smith', '12345678901', 'Responsabile'),
+('user3@example.com', 'password3', 'Bob', 'Johnson', '98765432101', 'Subordinatp'),
+('user4@example.com', 'password4', 'Alice', 'Brown', '11223344556', 'Dirigente');
+
+INSERT INTO Turno (giorno, ora_inizio, ora_fine) VALUES
+('2024-01-16', '08:00', '12:00'),
+('2024-01-17', '13:00', '17:00'),
+('2024-01-18', '09:30', '16:30');
+
+INSERT INTO AssegnatoA (id_turno, id_utente) VALUES
+(1, 'user2@example.com'),
+(2, 'user3@example.com'),
+(3, 'user4@example.com'),
+(3, 'user1@example.com');
+
+INSERT INTO Permesso (dal_giorno, al_giorno, motivo, stato, richiedente_email) VALUES
+('2024-01-20', '2024-01-22', 'Vacanza', true, 'user2@example.com'),
+('2024-02-05', '2024-02-08', 'Formazione', null, 'user3@example.com'),
+('2024-03-10', '2024-03-12', 'Malattia', false, 'user4@example.com'),
+('2024-04-15', '2024-04-17', 'Conferenza', null, 'user1@example.com');
+
+INSERT INTO Progetto (nome, descrizione, obbiettivi, scadenza, avvisi, budget, numDipendenti, responsabile_email, stato, idAzienda) VALUES
+('Progetto1', 'Descrizione del Progetto1', 'Obiettivi del Progetto1', '2024-05-31', 'Avvisi Progetto1', 50000.00, 5, 'user1@example.com', false, '12345678901'),
+('Progetto2', 'Descrizione del Progetto2', 'Obiettivi del Progetto2', '2024-06-30', 'Avvisi Progetto2', 75000.00, 8, 'user2@example.com', true, '98765432101'),
+('Progetto3', 'Descrizione del Progetto3', 'Obiettivi del Progetto3', '2024-07-15', 'Avvisi Progetto3', 100000.00, 10, 'user3@example.com', false, '11223344556');
+
+INSERT INTO LavoraA (email, id_progetto) VALUES
+('user1@example.com', 1),
+('user2@example.com', 2),
+('user3@example.com', 3),
+('user4@example.com', 1),
+('user4@example.com', 3);
+
+INSERT INTO Task (descrizione, stato, id_progetto, subordinato_email) VALUES
+('Task1 del Progetto1', false, 1, 'user2@example.com'),
+('Task2 del Progetto2', true, 2, 'user3@example.com'),
+('Task3 del Progetto3', false, 3, 'user4@example.com'),
+('Task4 del Progetto1', true, 1, 'user1@example.com'),
+('Task5 del Progetto2', false, 2, 'user2@example.com');
